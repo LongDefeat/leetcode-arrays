9e63e6617af6c8fc8adc648ef93a49c5c972ac70
Mason Sams <78603508+LongDefeat@users.noreply.github.com>
2023-07-13 17:55:23 -0700

    Update arrays.js with new problem

--- arrays.js
+++ arrays.js
@@ -176,3 +176,30 @@ let findMaxSumSubarray = function (nums) {
   }
   return maxSum;
 };
+/* Insert Interval: given an array of intervals, insert a new interval that will
+update any overlapping intervals*/
+var insert = function(intervals, newInterval) {
+  let result = [];
+  
+  for (let i = 0; i < intervals.length; i++) {
+      let interval = intervals[i];
+      
+      // If overlaps
+      if (Math.max(interval[0], newInterval[0]) <= Math.min(interval[1], newInterval[1])) {
+          newInterval = [Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])];
+          continue;
+      }
+      
+      // If lower
+      if (interval[0] > newInterval[1]) {
+          result.push(newInterval, ...intervals.slice(i));
+          return result;
+      }
+      
+      result.push(interval);
+  }
+  
+  result.push(newInterval);
+  return result;
+};
+
